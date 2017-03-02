e07a4402b79687bc292bd005f81fc18c994b13e0
BrianOjeda <brian_ezequiel_ojeda@hotmail.com>
2017-03-02 20:04:51 -0300

    Primer comit

---
+++ www/js/controllers.js
@@ -0,0 +1,68 @@
+angular.module('app.controllers', [])
+  
+
+
+.controller('CargaCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+	
+}])
+
+.controller('polloCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('choriCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+      
+.controller('loginCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('homeCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('cerdoCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('vacaCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('embuCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+ 
---
+++ www/js/routes.js
@@ -0,0 +1,91 @@
+angular.module('app.routes', [])
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+    
+  
+
+      .state('tabsController.pollo', {
+    url: '/pollo',
+    views: {
+      'pollo': {
+        templateUrl: 'templates/pollo.html',
+        controller: 'polloCtrl'
+      }
+    }
+  })
+
+  .state('tabsController.chori', {
+    url: '/chori',
+    views: {
+      'chori': {
+        templateUrl: 'templates/chori.html',
+        controller: 'choriCtrl'
+      }
+    }
+  })
+
+  .state('tabsController', {
+    url: '/tabs',
+    templateUrl: 'templates/tabsController.html',
+    abstract:true
+  })
+
+  .state('login', {
+    url: '/login',
+    templateUrl: 'templates/login.html',
+    controller: 'loginCtrl'
+  })
+
+  .state('home', {
+    url: '/homecliente',
+    templateUrl: 'templates/home.html',
+    controller: 'homeCtrl'
+  })
+
+  .state('tabsController.cerdo', {
+    url: '/cerdo',
+    views: {
+      'tab1': {
+        templateUrl: 'templates/cerdo.html',
+        controller: 'cerdoCtrl'
+      }
+    }
+  })
+
+  .state('tabsController.vaca', {
+    url: '/vaca',
+    views: {
+      'tab4': {
+        templateUrl: 'templates/vaca.html',
+        controller: 'vacaCtrl'
+      }
+    }
+  })
+
+  .state('tabsController.embu', {
+    url: '/embutido',
+    views: {
+      'tab5': {
+        templateUrl: 'templates/embu.html',
+        controller: 'embuCtrl'
+      }
+    }
+  })
+   .state('carga', {
+    url: '/carga',
+    templateUrl: 'templates/carga.html',
+    controller: 'CargaCtrl'
+  })
+ 
+
+$urlRouterProvider.otherwise('/login')
+
+  
+
+});
